36b69ac8d31f0ccb84574e7a11f44f655301dacb
Javonn Liner <57652233+javonn13@users.noreply.github.com>
2021-06-27 12:59:50 -0700

    Update source.py

--- source.py
+++ source.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 con = mysql.connector.connect(
-user = "ardit700_student",
-password = "ardit700_student",
-host = "108.167.140.122",
-database = "ardit700_pm1database"
+user = "a0_student",
+password = "a_student",
+host = "111.111.111.111",
+database = "_______pm1database"
 )
 
 cursor = con.cursor()
